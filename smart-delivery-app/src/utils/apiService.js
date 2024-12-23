b1245ab2b9f25282c4e14abdd53e8a893c287d82
Devarsh Chandiwade <119281406+IMxSPYDER@users.noreply.github.com>
2024-12-23 16:12:53 +0530

    Update apiService.js

--- smart-delivery-app/src/utils/apiService.js
+++ smart-delivery-app/src/utils/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api", // Replace with your backend URL
+  baseURL: https://rentkar-api.vercel.app/api", // Replace with your backend URL
 });
 
 // Example functions
